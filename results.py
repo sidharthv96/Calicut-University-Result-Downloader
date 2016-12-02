95cb9cffd21f725a41a05b00d5359a4e653595e0
sidharth_vinod <sidharthv96@gmail.com>
2016-12-02 09:52:55 +0530

    Error handling

--- results.py
+++ results.py
@@ -7,20 +7,23 @@ import os
 
 # Define a function for the thread
 def download_result(regno):
-    id = str(md5.new(str(random.random())).hexdigest())[:26]
     reg = "ETAOECS"+str(regno).zfill(3)
-    print reg,id
-    headers = {"Cookie": "PHPSESSID="+id}
-    data = {"regno": reg, 'sum': '111', 'id': '5659', 'sessionok': 'yes'}
+    try:
+        id = str(md5.new(str(random.random())).hexdigest())[:26]
+        print reg,id
+        headers = {"Cookie": "PHPSESSID="+id}
+        data = {"regno": reg, 'sum': '111', 'id': '5659', 'sessionok': 'yes'}
 
-    r = requests.post('http://61.0.254.181/CuPbhavan/res_newregentry.php', data=data, headers=headers)
-    r = requests.post('http://61.0.254.181/CuPbhavan/rs_newcheck.php', headers=headers)
-    r = requests.post('http://61.0.254.181/CuPbhavan/cubtech7/barch_res.php', headers=headers)
+        r = requests.post('http://61.0.254.181/CuPbhavan/res_newregentry.php', data=data, headers=headers)
+        r = requests.post('http://61.0.254.181/CuPbhavan/rs_newcheck.php', headers=headers)
+        r = requests.post('http://61.0.254.181/CuPbhavan/cubtech7/barch_res.php', headers=headers)
 
-    with open("PDF/"+str(regno)+".pdf", 'wb') as fd:
-        for chunk in r.iter_content(chunk_size=128):
-            fd.write(chunk)
-    print "Saved : "+reg
+        with open("PDF/"+str(regno)+".pdf", 'wb') as fd:
+            for chunk in r.iter_content(chunk_size=128):
+                fd.write(chunk)
+        print "Saved : "+reg
+    except:
+        print "Failed : "+reg        
 
 try:
     if not os.path.exists("PDF"):
